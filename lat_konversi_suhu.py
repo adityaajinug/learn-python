5cdc56d3319434b08db63baa5ad8e4278596c72e
Aditya Ajie Nugroho <adityaajinug@gmail.com>
2023-09-25 15:46:56 +0700

    add konversion kelvin to fahrenheit

--- lat_konversi_suhu.py
+++ lat_konversi_suhu.py
@@ -18,3 +18,17 @@ kelvin = celcius + 273
 print("suhu kelvin : ", kelvin, "K")
 
 
+# konversi fahrenheit ke kelvin
+temperatur_kelvin = ((5/9) * fahrenheit - 32) + 273
+print("suhu fahrenheit ke kelvin : ", temperatur_kelvin, "K")
+
+print("\nProgram Konversi Temperatur\n")
+
+kelvin = float(input("Masukkan suhu dalam Kelvin: "))
+print("Suhu Kelvin: ", kelvin, "K")
+
+# Konversi ke Fahrenheit
+fahrenheit = (kelvin - 273) * 9/5 + 32
+print("Suhu Fahrenheit: ", fahrenheit, "F")
+
+
